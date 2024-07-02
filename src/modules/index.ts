bc5c98363015ebc93ccb12ca7c54262bb1a46ab9
Adarsh031998 <adarsh@incaendo.com>
2024-07-02 16:24:43 +0530

    Made all components dynamic + remove unused components

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from './Header';
 export { default as ConfirmationModal } from './Modals/confirmation-modal';
 export { default as BaseModal } from './Modals/base-modal';
-export { default as AppTourConfirmModal } from './Modals/app-tour-confir-modal';
+export { default as AppTourConfirmModal } from './Modals/app-tour-confirm-modal';
 export { default as UppyUploader } from './Uploader';
